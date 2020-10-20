6bf3ea5c216f9b9a228692394acfff8e0db86d7c
Jordan Lambrecht <jordan@pixelbakery.co>
2020-10-20 12:29:27 -0500

    responsiveness

--- assets/backtotop.js
+++ assets/backtotop.js
@@ -15,3 +15,13 @@ $(document).ready(function(){
         return false;
     });
 });
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+); 
